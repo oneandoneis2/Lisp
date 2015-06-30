37f7b1ff12917819fe4ef1f753e9d604330033d1
Dominic Humphries <dominich@broadbean.com>
2015-06-30 16:59:42 +0100

    Add location describer

--- wiz_ad.lisp
+++ wiz_ad.lisp
@@ -1,7 +1,11 @@
 (defparameter *nodes*
   '(
-    (living-room (you are in the living room. A wizard is snoring on the couch) )
+    (living-room (You are in the living room. A wizard is snoring on the couch.) )
     (garden (You are in a lovely garden. There is a well in front of you.) )
     (attic (You are in the attic. There is a giant welding torch in the corner.) )
     )
+  )
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes))
   )
