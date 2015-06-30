cbdecf423372f85215d9e2be79ac9e207aa0f211
Dominic Humphries <dominich@broadbean.com>
2015-06-30 18:02:07 +0100

    Add function for objects at a location

--- wiz_ad.lisp
+++ wiz_ad.lisp
@@ -54,3 +54,11 @@
                   )
           )
   )
+
+(defun objects-at (loc objs obj-locs)
+  (labels (
+           (at-loc-p (obj) (eq (cadr (assoc obj obj-locs)) loc) )
+           )
+    (remove-if-not #'at-loc-p objs)
+   )
+ )
