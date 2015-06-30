19d9668193a84b22b5990e25a070f711d2b315c6
Dominic Humphries <dominich@broadbean.com>
2015-06-30 16:48:58 +0100

    Begin the wizard's adventure game

---
+++ wiz_ad.lisp
@@ -0,0 +1,7 @@
+(defparameter *nodes*
+  '(
+    (living-room (you are in the living room. A wizard is snoring on the couch) )
+    (garden (You are in a lovely garden. There is a well in front of you.) )
+    (attic (You are in the attic. There is a giant welding torch in the corner.) )
+    )
+  )
