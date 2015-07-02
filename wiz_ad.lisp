ece0eb393381f8c9916b769c04a83e59f02cd56d
Dominic Humphries <dominich@broadbean.com>
2015-07-02 15:22:38 +0100

    Add clever output printing

--- wiz_ad.lisp
+++ wiz_ad.lisp
@@ -133,3 +133,32 @@
     `(I do not know how to ,(car sexp))
     )
  )
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst))
+          )
+      (cond ((eql item #\space) (cons item (tweak-text rest caps lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eql item #\") (tweak-text rest caps (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+            (caps (cons (char-upcase item) (tweak-text rest nil lit)))
+            (t (cons (char-downcase item) (tweak-text rest nil nil)))
+            )
+      )
+    )
+  )
+
+(defun game-print (lst)
+  (princ (
+          coerce (
+                  tweak-text (
+                             coerce (
+                                     string-trim "() " (prin1-to-string lst)
+                                     ) 'list
+                             ) t nil
+                  ) 'string)
+         )
+  (fresh-line)
+  )
