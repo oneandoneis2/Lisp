825fadaebfd8f46e85c1cbc42ab72655724d66b4
Dominic Humphries <dominich@broadbean.com>
2015-07-11 18:23:32 +0100

    Added lisp primitives

---
+++ primitives.lisp
@@ -0,0 +1,24 @@
+(defun null. (x)
+  (eq x '()))
+
+(defun and. (x y)
+  (cond (x (cond (y 't) ('t '())))
+        ('t '())))
+
+(defun not. (x)
+  (cond (x '())
+        ('t 't)))
+
+(defun append. (x y)
+  (cond ((null. x) y)
+        ('t (cons (car x) (append. (cdr x) y)))))
+
+(defun pair. (x y)
+  (cond ((and. (null. x) (null. y)) '())
+        ((and. (not. (atom x)) (not. (atom y)))
+         (cons (list (car x) (car y))
+               (pair. (cdr x) (cdr y))))))
+
+(defun assoc. (x y)
+  (cond ((eq x (caar y))(cadar y))
+        ('t (assoc. x (cdr y)))))
