42b579c19ffada5488d13f6396a88138600de53a
Dominic Humphries <dominich@broadbean.com>
2015-07-18 14:58:03 +0100

    Begin the Orc Battle game

---
+++ orc.lisp
@@ -0,0 +1,6 @@
+(defparameter *player-heath* nil)
+(defparameter *player-agility* nil)
+(defparameter *player-strength* nil)
+(defparameter *monsters* nil)
+(defparameter *monster-builders* nil)
+(defparameter *monster-num* 12)
