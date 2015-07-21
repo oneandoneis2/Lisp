f8cfab7b9f7e37638c258a19dd22616167ec6e73
Dominic Humphries <dominich@broadbean.com>
2015-07-21 17:23:39 +0100

    Add plants

--- evolution.lisp
+++ evolution.lisp
@@ -2,3 +2,13 @@
 (defparameter *height* 30)
 (defparameter *jungle* '(45 10 10 10))
 (defparameter *plant-energy* 80)
+(defparameter *plants* (make-hash-table :test #'equal))
+
+(defun random-plant (left top width height)
+  (let ((post (cons (+ left (random width)) (+ top (random height)))))
+    (setf (gethash pos *plants*) t)))
+
+(defun add-plants ()
+  (apply #'random-plant *jungle*)
+  (random-plant 0 0 *width* *height*))
+
