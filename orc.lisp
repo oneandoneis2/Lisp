f72d5be52ff5dc93bfcdfeb6d35cdc630938b637
Dominic Humphries <dominich@broadbean.com>
2015-07-21 11:30:10 +0100

    Add more setup functions

--- orc.lisp
+++ orc.lisp
@@ -70,3 +70,48 @@
     (if (monster-dead m)
       (random-monster)
       m)))
+
+(defun pick-monster ()
+  (fresh-line)
+  (princ "Monster #:")
+  (let ((x (read)))
+    (if (not (and (integerp x) (>= x 1) (<= x *monster-num*)))
+      (progn (princ "That is not a valid monster.")
+             (pick-monster))
+      (let ((m (aref *monsters* (1- x))))
+        (if (monster-dead m)
+          (progn (pinc "That monster is already dead.")
+                 (pick-monster))
+          m)))))
+
+(defun init-monsters ()
+  (setf *monsters*
+        (map 'vector
+             (lambda (x)
+               (funcall (nth (random (length *monster-builders*))
+                             *monster-builders*)))
+             (make-array *monster-num*))))
+
+(defun monster-dead (m)
+  (<= (monster-health) 0))
+
+(defun monsters-dead ()
+  (every #'monster-dead *monsters*))
+
+(defun show-monsters ()
+  (fresh-line)
+  (princ "Your foes:")
+  (let ((x 0))
+    (map 'list
+         (lambda (m)
+           (fresh-line)
+           (princ "  ")
+           (princ (incf x))
+           (princ ". ")
+           (if (monster-dead m)
+             (princ "*dead*")
+             (progn (princ "(Health =")
+                    (princ (monster-health m))
+                    (princ ") ")
+                    (monster-show m))))
+         *monsters*)))
