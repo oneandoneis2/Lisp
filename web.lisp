a43e092436deab6decbc6bf91598fe1f8ca58fba
Dominic Humphries <dominich@broadbean.com>
2015-07-30 12:12:17 +0100

    Working "hello world" web server

--- web.lisp
+++ web.lisp
@@ -64,3 +64,10 @@
                 (funcall request-handler path header params))))
       (socket-server-close socket))))
 
+(defun hello-request-handler (path header params)
+  (if (equal path "greeting")
+    (let ((name (assoc 'name params)))
+      (if (not name)
+        (princ "<html><form>What is your name?<input name='name'></form></html>")
+        (format t "<html>Nice to meet you, ~a!</html>" (cdr name))))
+    (princ "Sorry.. I don't know that page.")))
