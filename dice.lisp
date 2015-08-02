931479453d717aace666f2ed8ecff7f3b0cf5c9e
Dominic Humphries <dominich@broadbean.com>
2015-08-02 15:18:47 +0100

    Start adding game logic

--- dice.lisp
+++ dice.lisp
@@ -24,3 +24,56 @@
                         do (format t "~a-~a "
                                    (player-letter (first hex))
                                    (second hex))))))
+
+(defun game-tree (board player spare-dice first-move)
+  (list player
+        board
+        (add-passing-move board
+                          player
+                          spare-dice
+                          first-move
+                          (attacking-moves board player spare-dice))))
+
+(defun add-passing-move (board player spare-dice first-move moves)
+  (if first-move
+    moves
+    (cons (list nil
+                (game-tree (add-new-dice board player (1- spare-dice))
+                           (mod (1+ player) *num-players*)
+                           0
+                           t))
+          moves)))
+
+(defun attacking-moves (board cur-player spare-dice)
+  (labels ((player (pos)
+                   (car (aref board pos)))
+           (dice (pos)
+                 (cadr (aref board pos))))
+    (mapcan (lambda (src)
+              (when (eq (player src) cur-player)
+                (mapcan (lambda (dst)
+                          (when (and (not (eq (plasyer dst) cur-player))
+                                     (> (dice src) (dice dst)))
+                            (list
+                              (list (list src dst)
+                                    (game-tree (board-attack board
+                                                             cur-player
+                                                             src
+                                                             dst
+                                                             (dice src))
+                                               cur-player
+                                               (+ spare-dice (dice dst))
+                                               nil)))))
+                        (neighbours src))))
+            (loop for n below *board-hexnum* collect n))))
+
+(defun neighbours (pos)
+  (let ((up (- pos *board-size*))
+        (down (+ pos *board-size*)))
+    (loop for p in (append (list up down)
+                           (unless (zerop (mod pos *board-size*))
+                             (list (1- up) (1- pos)))
+                           (unless (zerop (mod (1+ pos) *board-size*))
+                             (list (1+ pos) (1+ down))))
+          when (and (>= p 0) (< p *board-hexnum*))
+          collect p)))
