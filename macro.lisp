961b2064db023a8448bcf12d06c9f30fc5145f0c
Dominic Humphries <dominich@broadbean.com>
2015-08-08 17:47:12 +0100

    First macro

---
+++ macro.lisp
@@ -0,0 +1,11 @@
+(defmacro let1 (var val &body body)
+  `(let ((,var ,val))
+     ,@body))
+; (let ((foo (+ 2 3))) (* foo foo))
+; (macroexpand '(let1 foo (+ 2 3) (* foo foo)))
+; (LET ((FOO (+ 2 3))) (* FOO FOO))
+
+(defun add (a b)
+  (let1 x (+ a b)
+        (format t "The sum is ~a" x)
+        x))
