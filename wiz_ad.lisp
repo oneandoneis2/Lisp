0aac277c553136f8f25e63847bd971497e5cb90d
Dominic Humphries <dominich@broadbean.com>
2015-08-30 14:12:09 +0100

    Can now weld chain to bucket and dunk it in the well

--- wiz_ad.lisp
+++ wiz_ad.lisp
@@ -112,3 +112,35 @@
                   t
                   nil) 'string))
   (fresh-line))
+
+(defun have (object)
+  (member object (cdr (inventory))))
+
+(defparameter *chain-welded* nil)
+
+(defun weld (subject object)
+  (if (and (eq *location* 'attic)
+           (eq subject 'chain)
+           (eq object 'bucket)
+           (have 'chain)
+           (have 'bucket)
+           (not *chain-welded*))
+    (progn (setf *chain-welded* t)
+           '(the chain is now welded to the bucket))
+    '(you cannot weld like that.)))
+
+(pushnew 'weld *allowed-commands*)
+
+(defparameter *bucket-filled* nil)
+
+(defun dunk (subject object)
+  (if (and (eq *location* 'garden)
+           (eq subject 'bucket)
+           (eq object 'well)
+           (have 'bucket)
+           *chain-welded*)
+    (progn (setf *bucket-filled* t)
+           '(the bucket is now full of water))
+    '(you cannot dunk like that)))
+
+(pushnew 'dunk *allowed-commands*)
